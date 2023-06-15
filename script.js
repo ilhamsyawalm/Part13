f75bb7c977d403a2f8388a8adabed5adac84e42b
Zhephyr <ilhamsyawal87@gmail.com>
2023-06-15 17:25:22 +0700

    menyelesaikan perhitungan dengan mengambil seluruh data dari html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ! Harga tiap makanan
-const hargaAlpukat = document.getElementById("totalAlpukat");
-const hargaBlueberry = document.getElementById("totalBlueberry");
-const hargaOrange = document.getElementById("totalOrange");
+const a = document.querySelector(".harga-alpukat");
+const b = document.querySelector(".harga-blueberry");
+const o = document.querySelector(".harga-orange");
 
 // ! Input harga
 const nilaiAlpukat = document.querySelector(".dibeli input[name=alpukat]");
@@ -9,19 +9,27 @@ const nilaiBlueberry = document.querySelector(".dibeli input[name=blueberry]");
 const nilaiOrange = document.querySelector(".dibeli input[name=orange]");
 const banyak = document.querySelectorAll(".dibeli input");
 
+// ! Harga total makanan
+const hargaAlpukat = document.getElementById("totalAlpukat");
+const hargaBlueberry = document.getElementById("totalBlueberry");
+const hargaOrange = document.getElementById("totalOrange");
+
 // !Payment
 const sub = document.getElementById("sub");
 const ppn = document.getElementById("ppn");
-const ongkir = 10000;
-const potongan = 5000;
+const ongkir = document.getElementById("ongkir");
+const potongan = document.getElementById("potongan");
 const totalHarga = document.getElementById("totalHarga");
+
+console.log(ongkir.textContent);
+console.log(potongan.textContent);
 
 banyak.forEach(function (pil) {
   pil.addEventListener("input", function () {
     // Untuk harga tiap buah
-    const alpuket = nilaiAlpukat.value * 10000;
-    const bluberry = nilaiBlueberry.value * 15000;
-    const oren = nilaiOrange.value * 12000;
+    const alpuket = nilaiAlpukat.value * a.textContent;
+    const bluberry = nilaiBlueberry.value * b.textContent;
+    const oren = nilaiOrange.value * o.textContent;
 
     hargaAlpukat.innerHTML = alpuket;
     hargaBlueberry.innerHTML = bluberry;
@@ -30,9 +38,13 @@ banyak.forEach(function (pil) {
     // Untuk payment
     const totalan = alpuket + bluberry + oren;
     const pajak = totalan * 0.1;
+    const ongkos = totalan * 0.2;
+    const potong = totalan * 0.2;
 
     sub.innerHTML = totalan;
     ppn.innerHTML = pajak;
-    totalHarga.innerHTML = totalan + pajak + ongkir + potongan;
+    ongkir.innerHTML = ongkos;
+    potongan.innerHTML = potong;
+    totalHarga.innerHTML = totalan + pajak + ongkos - potong;
   });
 });
